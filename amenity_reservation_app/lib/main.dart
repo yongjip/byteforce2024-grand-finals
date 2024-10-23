a0e0078e1f2da3eed9cd8cc231dc40be30f5ea9d
Yongjip Kim <yongjip@techtaka.com>
2024-10-23 11:12:30 +0900

    added .env

--- amenity_reservation_app/lib/main.dart
+++ amenity_reservation_app/lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load environment variables
+  await dotenv.load(fileName: ".env");
+
+  // Initialize Supabase with environment variables
   await Supabase.initialize(
-    url: 'https://hcpnmzzdthfnhjashhgr.supabase.co', // Your Supabase URL
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhjcG5tenpkdGhmbmhqYXNoaGdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk0NzM4MTYsImV4cCI6MjA0NTA0OTgxNn0.TiVdRYb-qBMND7VtADMdyGIULD3gtChmMzh7N8mJZhs', // Your Supabase Anon Key
+    url: dotenv.env['SUPABASE_URL'] ?? '', // Your Supabase URL
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'] ?? '', // Your Supabase Anon Key
   );
   runApp(MyApp());
 }
